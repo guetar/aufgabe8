7ccea3c3bf6cac5e39652a1e28da80fb1aae716b
gbernsteiner <guetar@gmx.at>
2012-12-08 16:39:06 +0100

    Annotation!

--- Test.java
+++ Test.java
@@ -3,6 +3,17 @@
  *
  * @author guetar
  */
+
+@ClassPreamble (
+   author = "John Doe",
+   date = "3/17/2002",
+   currentRevision = 6,
+   lastModified = "4/12/2004",
+   lastModifiedBy = "Jane Doe",
+   // Note array notation
+   reviewers = {"Alice", "Bob", "Cindy"}
+)
+
 public class Test {
     
     public static void main(String[] args) {
--- Tractor.java
+++ Tractor.java
@@ -6,14 +6,24 @@
 public abstract class Tractor extends Node {
     
     private final int nr;
+    private int hours;
     
     public Tractor(int nr) {
         super("tractor_" + nr);
         this.nr = nr;
+        this.hours = 0;
     }
     
     public int getNr() {
         return nr;
+    }
+    
+    public void raiseHours() {
+        hours++;
+    }
+    
+    public int getHours() {
+        return hours;
     }
     
     public abstract int getAmount();
