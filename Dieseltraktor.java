11887873032d83f9379a1ed8db9e6116d0dd03bc
steff <steff@ono-sendai>
2012-12-08 15:52:34 +0100

    bla

--- Dieseltraktor.java
+++ Dieseltraktor.java
@@ -4,4 +4,16 @@
  */
 public class Dieseltraktor extends Traktor {
     
+    public 
+
+    @Override
+    public int getAmount() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public int raiseAmount() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
 }
