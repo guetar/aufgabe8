4d01ecf2419d4bf3dd226da17f381508427ad0c5
gbernsteiner <guetar@gmx.at>
2012-12-08 17:37:23 +0100

    Hours in Use!

--- Tractor.java
+++ Tractor.java
@@ -9,30 +9,18 @@
 public abstract class Tractor extends Node {
     
     private final int nr;
-    private int hours;
     private Tool tool;
     private int hoursInUse;
     
     public Tractor(int nr) {
         super("tractor_" + nr);
         this.nr = nr;
-        this.hours = 0;
+        this.hoursInUse = 0;
     }
     
     public int getNr() {
         return nr;
     }
-    
-    public void raiseHours() {
-        hours++;
-    }
-    
-    public int getHours() {
-        return hours;
-    }
-    
-    public abstract int getAmount();
-    public abstract int raiseAmount();
     
     public void changeTool(Tool t) {
         tool = t;
